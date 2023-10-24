65fedf08af10bf1f723aef5f482f07c5cb271317
anilhavale <77318756+anilhavale@users.noreply.github.com>
2023-10-24 12:42:24 +0530

    First Commit On GithubDemo project

---
+++ src/Github1/Test1.java
@@ -0,0 +1,17 @@
+package Github1;
+
+public class Test1 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		String a="Intelligent";
+		
+		if(a=="Intelligent")
+		{
+			System.out.println("test is pass : ");
+		}
+	}
+	
+}
